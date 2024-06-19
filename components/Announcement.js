cc8333103f4b178172e74bf42657383988c20680
AbdurRahmanFarooqi <152776099+AbdurRahmanFarooqi@users.noreply.github.com>
2024-06-19 05:48:24 +0500

    Admin Panel
    Admin panel for Ez-Transit

---
+++ components/Announcement.js
@@ -0,0 +1,14 @@
+// src/components/Announcement.js
+import React from 'react';
+import '../styles/components/Announcement.css';
+
+const Announcement = () => {
+  return (
+    <div className="announcement-container">
+      <h2>Announcements</h2>
+      {/* Add your announcement UI here */}
+    </div>
+  );
+};
+
+export default Announcement;
---
+++ components/TopupApproval.js
@@ -0,0 +1,14 @@
+// src/components/TopupApproval.js
+import React from 'react';
+import '../styles/components/TopupApproval.css';
+
+const TopupApproval = () => {
+  return (
+    <div className="topup-approval-container">
+      <h2>Topup Approvals</h2>
+      {/* Add your topup approval UI here */}
+    </div>
+  );
+};
+
+export default TopupApproval;
